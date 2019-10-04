515af10b9279b535dac9a4054e3befa10de6cab5
Opspark Lizard <opsparklizard@Opsparks-MBP.attlocal.net>
2019-10-04 11:58:31 -0500

    semi-major dht updates

--- iot-projects/second-slice-of-pi/plugins/internal/dhtPlugin.js
+++ iot-projects/second-slice-of-pi/plugins/internal/dhtPlugin.js
@@ -1,6 +1,6 @@
 var resources = require('./../../resources/model');
 
-var sensor;
+var interval, sensor;
 var device = resources.pi.sensors.dht;
 var pluginName = resources.pi.sensors.dht.name;
 var localParams = {'frequency': 2000};
@@ -12,6 +12,7 @@ exports.start = function (params) {
 };
 
 exports.stop = function () {
+	clearInterval(interval);
 	console.info(pluginName + " plugin stopped!");
 };
 
@@ -25,13 +26,12 @@ function connectHardware() {
 			var readout = sensorDriver.read();
 			device.temperature.value = parseFloat(readout.temperature.toFixed(2));
 			device.humidity.value = parseFloat(readout.humidity.toFixed(2));
-			
-			setTimeout(function () {
-				sensor.read();
-			}, localParams.frequency);
 		}
 	};
 	sensor.initialize();
 	sensor.read();
+	interval = setInterval(function () {
+		sensor.read();
+	}, localParams.frequency);
 }
 
