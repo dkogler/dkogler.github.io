8b88d8d21816e413727a56655e7f31cc1b2d4dcf
dkogler <danielk373@gmail.com>
2020-08-10 15:28:21 +0000

    updated script

--- script/setup.sh
+++ script/setup.sh
@@ -2,16 +2,10 @@
 read -p "Enter Your GitHub Username: "  username
 read -s -p "Enter Your GitHub Password: "  password
 
-cd ..
 git clone https://${username}:${password}@github.com/operationspark/circularity
-git clone https://github.com/operationspark/first-website
-git clone https://github.com/operationspark/portfolio
 git clone https://github.com/operationspark/platformer
 git clone https://github.com/operationspark/bouncing-box
-cd first-website
-rm -rf .git* .master
-cd ../portfolio
-rm -rf .git* .master
+
 cd ../platformer
 rm -rf .git* .master
 bower install
