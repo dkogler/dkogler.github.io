9eac389370f4f341a0775a9bab2a5ee67bf30c99
dkogler <danielk373@gmail.com>
2020-08-12 18:27:41 +0000

    save

--- script/setup.sh
+++ script/setup.sh
@@ -6,7 +6,6 @@ mkdir projects
 cd projects
 
 git clone https://${username}:${password}@github.com/operationspark/circularity
-git clone https://${username}:${password}@github.com/operationspark/runtime
 git clone https://github.com/operationspark/platformer
 git clone https://github.com/operationspark/bouncing-box
 
@@ -16,9 +15,6 @@ bower install
 cd ../bouncing-box
 rm -rf .git* .master
 cd ../circularity
-rm -rf .git* .master
-bower install
-cd ../runtime
 rm -rf .git* .master
 bower install
 
