885a5996fd654b292e7a0f44cc80269b5928b626
dkogler <danielk373@gmail.com>
2020-09-23 19:16:29 +0000

    adding bb-refactor

---
+++ asd-projects/bouncing-box-refactor/index.js
@@ -0,0 +1,79 @@
+/* global $ */
+'use strict'
+$(document).ready(function(){
+    //////////////////////////////////////////////////////////////////
+    //////////////////////////// SETUP ///////////////////////////////
+    //////////////////////////////////////////////////////////////////
+
+    var BOARD_WIDTH = $('#board').width();	// Number: the maximum X-Coordinate of the screen
+
+    // Every 50 milliseconds, call the update Function (see below)
+    setInterval(update, 50);
+
+    // Every time the box is clicked, call the handleBoxClick Function (see below)
+    $('#box').on('click', handleBoxClick);
+
+    var positionX = 0;
+    var speedX = 10;
+    var points = 0;
+
+
+    //////////////////////////////////////////////////////////////////
+    ////////////////////////// CORE LOGIC ////////////////////////////
+    //////////////////////////////////////////////////////////////////
+
+    /* 
+    This Function will be called 20 times/second. Each time it is called,
+    it should move the Box to a new location. If the box drifts off the screen
+    turn it around! 
+    */
+    function update() {
+        updatePosition();
+        checkBounds();
+    }
+
+    /* 
+    This Function will be called each time the box is clicked. Each time it is called,
+    it should increase the points total, increase the speed, and move the box to
+    the left side of the screen.
+    */
+    function handleBoxClick() {
+        increasePoints();
+        increaseSpeed();
+        resetPosition();
+    }
+
+    //////////////////////////////////////////////////////////////////
+    /////////////////////// HELPER FUNCTIONS /////////////////////////
+    //////////////////////////////////////////////////////////////////
+
+    // update helper functions
+    function updatePosition(){
+        positionX += speedX;
+        $('#box').css("left", positionX);
+    }
+    function checkBounds(){
+        if (positionX > BOARD_WIDTH) {
+            speedX = -speedX;
+        }
+        else if (positionX < 0) {
+            speedX = -speedX;
+        }
+    }
+    // box click helper functions
+    function increasePoints(){
+        points += 1;
+        $('#box').text(points);
+    }
+    function increaseSpeed(){
+        if (speedX >= 0) {
+            speedX += 3;
+        } 
+        else if (speedX < 0) {
+            speedX -= 3;
+        }
+    }
+    function resetPosition(){
+        positionX = 0;
+    }
+}); // DO NOT DELETE THIS LINE OF CODE. ALL JAVASCRIPT ABOVE HERE
