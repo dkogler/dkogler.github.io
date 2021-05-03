2de03aa700590e9a94a4b9f56879a5bc122f018f
Daniel Kogler <danielk373@gmail.com>
2021-05-03 15:52:15 -0500

    cleaning up

--- test/test-server.js
+++
@@ -1,9 +0,0 @@
-var http = require("http");
-var port = 8686;
-
-http.createServer(function(req,res){
-  // handle response
-  res.writeHeader(200, {'Content-Type': 'text/plain'});
-  res.end("okey dokey lokey");
-}).listen(port);
-
